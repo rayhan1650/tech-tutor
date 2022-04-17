11b535817b661f608bcfa5dce78136260053143e
rayhan1650 <nm.rayhan.1650@gmail.com>
2022-04-17 16:49:20 +0600

    banner part done

--- src/Pages/Home/Home/Home.js
+++ src/Pages/Home/Home/Home.js
@@ -1,11 +1,20 @@
 import React from "react";
+import "./Home.css";
 import Feedbacks from "../Feedbacks/Feedbacks";
 import Services from "../Services/Services";
 
 const Home = () => {
   return (
     <div>
-      <h2>This is home</h2>
+      <div
+        className="my-banner d-flex justify-content-center align-items-center img-fluid"
+        style={{ backgroundImage: `url("Cover.jpg")` }}
+      >
+        <h1 className="text-white   fw-bold">
+          LEARN NEW THINGS <br /> IN COMFORT OF YOUR HOME
+        </h1>
+      </div>
+
       <Services />
       <Feedbacks />
     </div>

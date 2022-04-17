dc7c41ec7393391a64bebad09f3ae22eb2ba5070
rayhan1650 <nm.rayhan.1650@gmail.com>
2022-04-17 11:39:35 +0600

    set simple routes

--- src/App.js
+++ src/App.js
@@ -1,9 +1,22 @@
+import { Route, Routes } from "react-router-dom";
 import "./App.css";
+import About from "./Pages/About/About";
+import Blogs from "./Pages/Blogs/Blogs";
+import Home from "./Pages/Home/Home/Home";
+import Footer from "./Pages/Shared/Footer/Footer";
+import Header from "./Pages/Shared/Header/Header";
 
 function App() {
   return (
     <div>
-      <h1>Tech Tutor</h1>
+      <Header />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/home" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/blogs" element={<Blogs />} />
+      </Routes>
+      <Footer />
     </div>
   );
 }

99db770a1ef0640417889ac8ce9f1f6d7a925985
rayhan1650 <nm.rayhan.1650@gmail.com>
2022-04-18 16:14:48 +0600

    login with email and password done

--- src/Pages/Login/Login/Login.js
+++ src/Pages/Login/Login/Login.js
@@ -1,12 +1,31 @@
-import React from "react";
+import React, { useRef } from "react";
 import { Button, Form } from "react-bootstrap";
-import { Link } from "react-router-dom";
+import { useSignInWithEmailAndPassword } from "react-firebase-hooks/auth";
+import { Link, useNavigate } from "react-router-dom";
+import auth from "../../../firebase.init";
 import SocialLogin from "../SocialLogin/SocialLogin";
 import "./Login.css";
 
 const Login = () => {
+  const emailRef = useRef("");
+  const passwordRef = useRef("");
+  const [signInWithEmailAndPassword, user, loading, error] =
+    useSignInWithEmailAndPassword(auth);
+  const navigate = useNavigate();
+
+  if (loading) {
+    return <p>Loading...</p>;
+  }
+
+  if (user) {
+    navigate("/");
+  }
+
   const handleSubmit = (event) => {
     event.preventDefault();
+    const email = emailRef.current.value;
+    const password = passwordRef.current.value;
+    signInWithEmailAndPassword(email, password);
   };
   return (
     <div className="my-login mx-auto">
@@ -14,18 +33,29 @@ const Login = () => {
       <Form onSubmit={handleSubmit}>
         <Form.Group className="mb-3" controlId="formBasicEmail">
           <Form.Label>Email address</Form.Label>
-          <Form.Control type="email" placeholder="Enter email" required />
+          <Form.Control
+            ref={emailRef}
+            type="email"
+            placeholder="Enter email"
+            required
+          />
         </Form.Group>
 
         <Form.Group className="mb-3" controlId="formBasicPassword">
           <Form.Label>Password</Form.Label>
-          <Form.Control type="password" placeholder="Password" required />
+          <Form.Control
+            ref={passwordRef}
+            type="password"
+            placeholder="Password"
+            required
+          />
         </Form.Group>
 
         <Button variant="primary" type="submit">
           Login
         </Button>
       </Form>
+      {error ? <p className="text-danger">Error: {error.message}</p> : <p></p>}
       <p className="mt-2 my-link">
         New to Tech Tutor? <Link to="/signup">Please SignUp.</Link>{" "}
       </p>

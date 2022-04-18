b6abf234e20eb2e5f986f0de4616d335226bf335
rayhan1650 <nm.rayhan.1650@gmail.com>
2022-04-18 17:28:26 +0600

    implement social login

--- src/Pages/Login/SocialLogin/SocialLogin.js
+++ src/Pages/Login/SocialLogin/SocialLogin.js
@@ -1,7 +1,23 @@
 import React from "react";
+import { useSignInWithGoogle } from "react-firebase-hooks/auth";
 import { FcGoogle } from "react-icons/fc";
+import { useLocation, useNavigate } from "react-router-dom";
+import auth from "../../../firebase.init";
 
 const SocialLogin = () => {
+  const navigate = useNavigate();
+  let location = useLocation();
+  let from = location.state?.from?.pathname || "/";
+  const [signInWithGoogle, user, loading, error] = useSignInWithGoogle(auth);
+
+  if (loading) {
+    return <p>Loading...</p>;
+  }
+
+  if (user) {
+    navigate(from, { replace: true });
+  }
+
   return (
     <div>
       <div className="d-flex align-items-center">
@@ -9,10 +25,10 @@ const SocialLogin = () => {
         <p className="mt-2 mx-2">or</p>
         <div style={{ height: "1px" }} className="bg-primary w-50"></div>
       </div>
-      {/* {errorElement} */}
+      {error ? <p className="text-danger">Error: {error.message}</p> : <p></p>}
       <div className="d-flex flex-column w-75 mx-auto">
         <button
-          //   onClick={() => signInWithGoogle()}
+          onClick={() => signInWithGoogle()}
           className="btn btn-outline-info rounded-pill fw-bold"
         >
           <FcGoogle className="me-2 fs-4 " /> Google Sign In
